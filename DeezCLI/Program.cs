c4dd3bf7fbeb0a33a585ddc485e276bd70596710
Trevor <me@trev.app>
2024-05-05 17:26:26 -0400

    Add fancier logging to CLI

--- DeezCLI/Program.cs
+++ DeezCLI/Program.cs
@@ -5,6 +5,7 @@ using DeezNET;
 using DeezNET.Data;
 using DeezNET.Exceptions;
 using Newtonsoft.Json.Linq;
+using Spectre.Console;
 
 namespace DeezCLI;
 
@@ -41,11 +42,16 @@ public class DownloadCommand : ICommand
     [CommandOption("concurrent", 'c', Description = "The max amount of allowed concurrent track downloads.")]
     public int Concurrent { get; init; } = 3;
 
-    private DeezerClient client;
-
     public async ValueTask ExecuteAsync(IConsole console)
     {
-        client = await DeezerClient.Create(ARL);
+        IAnsiConsole ansiConsole = AnsiConsole.Create(new AnsiConsoleSettings
+        {
+            Ansi = AnsiSupport.Detect,
+            ColorSystem = ColorSystemSupport.Detect,
+            Out = new AnsiConsoleOutput(console.Output)
+        });
+
+        var client = await DeezerClient.Create(ARL);
         if (DeezerURL.TryParse(URL, out DeezerURL parsedUrl))
         {
             long[] tracks = await parsedUrl.GetAssociatedTracks(client, TopLimit);
@@ -59,7 +65,7 @@ public class DownloadCommand : ICommand
                     await semaphore.WaitAsync();
                     try
                     {
-                        await DoDownload(console, track);
+                        await DoDownload(ansiConsole, client, track);
                     }
                     finally
                     {
@@ -73,41 +79,48 @@ public class DownloadCommand : ICommand
         else
             throw new InvalidURLException("Failed to parse the given URL, unable to download.");
 
-        console.Output.WriteLine("Download complete.");
+        ansiConsole.Markup("[green]Download complete.[/]");
     }
 
-    private async Task DoDownload(IConsole console, long track)
+    private async Task DoDownload(IAnsiConsole console, DeezerClient client, long track)
     {
-        console.Output.WriteLine($"Downloading track {track}...");
-
         JToken page = await client.GWApi.GetTrackPage(track);
+        string songTitle = page["DATA"]!["SNG_TITLE"]!.ToString();
+        string artistName = page["DATA"]!["ART_NAME"]!.ToString();
+        string albumTitle = page["DATA"]!["ALB_TITLE"]!.ToString();
+
+        console.Write(new Rule($"[yellow]Track[/] {track} [yellow][/]").Justify(Justify.Center));
+        console.MarkupLine($"[bold yellow]TITLE:[/] {songTitle}");
+        console.MarkupLine($"[bold yellow]ARTIST:[/] {artistName}");
+        console.MarkupLine($"[bold yellow]ALBUM:[/] {albumTitle}");
+        console.Write(new Rule());
+
+
         byte[] trackData = await client.Downloader.GetRawTrackBytes(track, PreferredBitrate, Downloader.GetLowerFallbackBitrate(PreferredBitrate));
         if (Metadata)
-        {
             trackData = await client.Downloader.ApplyMetadataToTrackBytes(track, trackData);
-        }
 
         // TODO: could use variables like deemixgui to setup custom export pathing
 
-        string artistFolderPath = Path.Combine(OutputDir, CleanPath(page["DATA"]!["ART_NAME"]!.ToString()));
+        string artistFolderPath = Path.Combine(OutputDir, CleanPath(artistName));
         if (!Directory.Exists(artistFolderPath))
             Directory.CreateDirectory(artistFolderPath);
 
-        string albumFolderPath = Path.Combine(artistFolderPath, CleanPath(page["DATA"]!["ALB_TITLE"]!.ToString()));
+        string albumFolderPath = Path.Combine(artistFolderPath, CleanPath(albumTitle));
         if (!Directory.Exists(albumFolderPath))
             Directory.CreateDirectory(albumFolderPath);
 
-        string title = CleanPath(page["DATA"]!["SNG_TITLE"]!.ToString());
-        int trackNum = int.Parse(page["DATA"]!["TRACK_NUMBER"]!.ToString());
+        string title = CleanPath(songTitle);
+        int trackNum = (int)page["DATA"]!["TRACK_NUMBER"]!;
         string ext = PreferredBitrate == Bitrate.FLAC ? "flac" : "mp3";
         string outPath = Path.Combine(albumFolderPath, $"{trackNum:00} - {title}.{ext}");
 
         await File.WriteAllBytesAsync(outPath, trackData);
 
-        console.Output.WriteLine($"Finished track {track}...");
+        console.MarkupLine($"[yellow][[#]] Track[/] {track} [yellow]downloaded.[/]");
     }
 
-    private string CleanPath(string str)
+    private static string CleanPath(string str)
     {
         char[] invalid = Path.GetInvalidFileNameChars();
         for (int i = 0; i < invalid.Length; i++)
